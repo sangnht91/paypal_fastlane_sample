9dd6c520e7e1890a6ec279fb2ec662315b204e8d
sangnht91 <sangnht3005@yahoo.com>
2024-09-05 15:10:05 +0700

    update api relatedorder

--- publics/js/upsell.fastlane.js
+++ publics/js/upsell.fastlane.js
@@ -120,7 +120,7 @@ const CheckoutFastLane = async () => {
       .call(FastlaneElements.arrElmFastlaneSB)
       .forEach(async (item, index) => {
         // disabled upsell submit button
-        item.removeAttribute("disabled")
+        item.addAttribute("disabled", true)
 
         const elementWatermark = `
         <div class="fastlane-shipping-address-${index}" style="display: none !important; margin-top: 10px; width: 100%; display: flex; justify-content: space-between;">
@@ -215,7 +215,7 @@ const CheckoutFastLane = async () => {
     Array.prototype.slice
       .call(FastlaneElements.arrElmFastlaneSB)
       .forEach(async (item, index) => {
-        item.setAttribute("disabled", false)
+        item.removeAttribute("disabled")
         paymentComponent.render(`.fastlane-payment-info-${index}`);
       });
   };
